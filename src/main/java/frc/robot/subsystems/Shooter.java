5db41f140e2be1d1d6687e16cb0e6a8f39eb2a18
Yanjun Zhao <yanjunzhao81@gmail.com>
2021-07-13 20:31:36 -0400

    added farm11 button, removed unnecessary shooter lookup, added back driveForwardAuto, set drivetrain back to tank drive

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -93,9 +93,7 @@ public class Shooter extends SubsystemBase {
             // System.err.println("Using original hard-coded table instead");
 
             // always have an entry at 0 so that it has a chance of working at short distances
-            distanceLookUp.put(0.0, new Double[] {5500.0, 115.0});
             distanceLookUp.put(50.0, new Double[] {5500.0, 115.0});
-            distanceLookUp.put(74.0, new Double[] {5500.0, 100.0});
             distanceLookUp.put(123.6, new Double[] {6000.0, 80.0});
             // distanceLookUp.put(137.1, new Double[] {5500.0, 80.0});
             distanceLookUp.put(174.9, new Double[] {7500.0, 65.0});
