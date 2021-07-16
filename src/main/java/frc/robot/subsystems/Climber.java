660b84184b1efda4e007826a31c8951729933be1
Paul Rensing <prensing@ligerbots.org>
2021-07-16 15:26:44 -0400

    Cleanup the Climber subsystem code. No functional changes

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -17,23 +17,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
-    double currentShoulderAngle;
-    double requestedShoulderAngle;
-    double lastShoulderAngle;
-    boolean shoulderMovingDown = false;
-    public final CANSparkMax shoulder; // declare new motor
-    public final CANSparkMax winch; // declare new motor
+    // these really should be private, but don't want to change too much code.
+    public final CANSparkMax shoulder;  
     public DutyCycleEncoder shoulderEncoder;
 
-    boolean deployed = false;
-    boolean autoLevel = false;
-    double currentWinchHeight;
-    double requestedWinchHeight;
-    double shoulderSpeedUp = Constants.SHOULDER_SPEED_UP; // set shoulder movement speed
-    double shoulderSpeedHold = Constants.SHOULDER_SPEED_HOLD; //This is not enough to lift the intake, but wll hold it in place
-    double shoulderRateDown = Constants.SHOULDER_RATE_DOWN; // a little under 2 seconds to get from max height to min height
+    public final CANSparkMax winch;
     CANEncoder winchEncoder;
-    //SHOULDER ENCODER IS AT 0.44 WHEN DOWN ALL THE WAY
+
+    // boolean deployed = false;
+    boolean autoLevel = false;
+    double requestedShoulderAngle;
+    double requestedWinchHeight;
+    double lastShoulderAngle;
+    boolean shoulderMovingDown = false;
 
     private DriveTrain driveTrain;
 
@@ -43,14 +39,13 @@ public class Climber extends SubsystemBase {
         winch = new CANSparkMax(Constants.WINCH_MOTOR_CAN_ID, MotorType.kBrushless); //init motor type and can id
         winch.setIdleMode(IdleMode.kBrake);// set to break when the motor is speed 0
         shoulderEncoder = new DutyCycleEncoder(9);
-        currentShoulderAngle = shoulderEncoder.get();
         winchEncoder = new CANEncoder(winch);
         this.driveTrain = driveTrain;
     }
 
     @Override
     public void periodic(){
-        currentShoulderAngle = shoulderEncoder.get();
+        double currentShoulderAngle = shoulderEncoder.get();
         SmartDashboard.putNumber("Shoulder Ticks", shoulderEncoder.get());
 
         if (!autoLevel) {
@@ -58,16 +53,16 @@ public class Climber extends SubsystemBase {
                 // move up
                 if (currentShoulderAngle > Constants.SHOULDER_MAX_HEIGHT) {
                     // Hold Position
-                    shoulder.setVoltage(shoulderSpeedHold);
+                    shoulder.setVoltage(Constants.SHOULDER_SPEED_HOLD);
                 }
                 else {
                     if (currentShoulderAngle < requestedShoulderAngle) {
                         // Need to move up
-                        shoulder.setVoltage(shoulderSpeedUp);
+                        shoulder.setVoltage(Constants.SHOULDER_SPEED_UP);
                     }
                     else {
                         // hold postion
-                        shoulder.setVoltage(shoulderSpeedHold);
+                        shoulder.setVoltage(Constants.SHOULDER_SPEED_HOLD);
                     }
                 }
             }
@@ -81,13 +76,13 @@ public class Climber extends SubsystemBase {
                 else {
                     if (currentShoulderAngle < requestedShoulderAngle) {
                         // We've gone down far enough
-                        shoulder.setVoltage(shoulderSpeedHold);
+                        shoulder.setVoltage(Constants.SHOULDER_SPEED_HOLD);
                         shoulder.setIdleMode(IdleMode.kBrake);
                     }
                     else {
                         // Now we get clever. We have to only allow it to fall at the
-                        // shoulderRateDown
-                        if ( lastShoulderAngle - currentShoulderAngle > shoulderRateDown * 0.02) {
+                        // Constants.SHOULDER_RATE_DOWN
+                        if (lastShoulderAngle - currentShoulderAngle > Constants.SHOULDER_RATE_DOWN * 0.02) {
                             // It's going too fast so we need to slow it down
                             shoulder.setIdleMode(IdleMode.kBrake);
                         }
@@ -99,15 +94,16 @@ public class Climber extends SubsystemBase {
                     }
                 }
             }
-        }
-        // Auto levelling
-        else {
+        } 
+        else
+        {
+            // Auto levelling
+
             System.out.println(" " + driveTrain.getPitch());
-            if (driveTrain.getPitch() > Constants.ROBOT_PITCH_ANGLE_FOR_CLIMB ) {
+            if (driveTrain.getPitch() > Constants.ROBOT_PITCH_ANGLE_FOR_CLIMB) {
                 // Need to lift the front
                 shoulder.setVoltage(Constants.SHOULDER_SPEED_LEVEL);
-            }
-            else {
+            } else {
                 // Hold where it is
                 shoulder.setIdleMode(IdleMode.kBrake);
                 shoulder.setVoltage(0.0);
@@ -118,13 +114,10 @@ public class Climber extends SubsystemBase {
 
         // Now let's do the winch
 
-        currentWinchHeight = winchEncoder.getPosition();
-
-        if (requestedWinchHeight < currentWinchHeight) {
+        if (winchEncoder.getPosition() >= requestedWinchHeight) {
             // Just stop the winch
             stopWinch();
-        }
-        else {
+        } else {
             winch.setVoltage(Constants.WINCH_SPEED_FAST);
         }
     }
@@ -146,54 +139,53 @@ public class Climber extends SubsystemBase {
         return winchEncoder.getPosition();
     }
 
-   public void resetWinchEncoder(){
-       winchEncoder.setPosition(0);
-   }
-
-   public double getShoulderPosition(){
-       return shoulderEncoder.get();
-   }
-
-   public void moveShoulder(final double angle) {
-    // This just sets parameters to be used in the periodic() method.
-    // Moving the shoulder to the correct angle will be done in the periodic() method
-
-    requestedShoulderAngle = angle;
-
-    // Limit max requested height
-    if (requestedShoulderAngle > Constants.SHOULDER_MAX_HEIGHT) {
-        requestedShoulderAngle = Constants.SHOULDER_MAX_HEIGHT;
+    public void resetWinchEncoder() {
+        winchEncoder.setPosition(0);
     }
 
-    // Limit min requested heght
-    if (requestedShoulderAngle < Constants.SHOULDER_MIN_HEIGHT) {
-        requestedShoulderAngle = Constants.SHOULDER_MIN_HEIGHT;
+    public double getShoulderPosition() {
+        return shoulderEncoder.get();
     }
 
-    if (requestedShoulderAngle > currentShoulderAngle) {
-        shoulderMovingDown = false;
+    public void moveShoulder(final double angle) {
+        // This just sets parameters to be used in the periodic() method.
+        // Moving the shoulder to the correct angle will be done in the periodic() method
+
+        requestedShoulderAngle = angle;
+
+        // Limit max requested height
+        if (requestedShoulderAngle > Constants.SHOULDER_MAX_HEIGHT) {
+            requestedShoulderAngle = Constants.SHOULDER_MAX_HEIGHT;
+        }
+
+        // Limit min requested heght
+        if (requestedShoulderAngle < Constants.SHOULDER_MIN_HEIGHT) {
+            requestedShoulderAngle = Constants.SHOULDER_MIN_HEIGHT;
+        }
+
+        if (requestedShoulderAngle > shoulderEncoder.get()) {
+            shoulderMovingDown = false;
+        } else {
+            shoulderMovingDown = true;
+        }
+        SmartDashboard.putBoolean("Moving Down", shoulderMovingDown);
+        SmartDashboard.putNumber("Shoulder Requested Angle", requestedShoulderAngle);
     }
-    else {
-        shoulderMovingDown = true;
+
+    public boolean shoulderBelowHeight(double degrees) {
+        // SHOULDER_MIN_HEIGHT is reference. Divide degrees by 360 to get encoder value
+        return shoulderEncoder.get() < Constants.SHOULDER_MIN_HEIGHT + degrees / 360.0;
     }
-    SmartDashboard.putBoolean("Moving Down", shoulderMovingDown);
-    SmartDashboard.putNumber("Shoulder Requested Angle", requestedShoulderAngle);
-}
 
-   public boolean shoulderBelowHeight(double degrees){
-    // SHOULDER_MIN_HEIGHT is reference. Divide degrees by 360 to get encoder value
-        return shoulderEncoder.get() < Constants.SHOULDER_MIN_HEIGHT + degrees/360.0;
-   }
+    public void autoLevel(boolean autoLevel) {
+        this.autoLevel = autoLevel;
+    }
 
-   public void autoLevel(boolean autoLevel){
-       this.autoLevel = autoLevel;
-   }
+    public boolean autoLeveling() {
+        return autoLevel;
+    }
 
-   public boolean autoLeveling(){
-       return autoLevel;
-   }
-
-   public boolean shoulderOnTarget () {
-       return Math.abs(requestedShoulderAngle - shoulderEncoder.get()) <= 0.05;
-   }
+    public boolean shoulderOnTarget() {
+        return Math.abs(requestedShoulderAngle - shoulderEncoder.get()) <= 0.05;
+    }
 }
